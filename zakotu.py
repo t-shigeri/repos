9586c3a16451574623bf96c37ab1b71f35dc1d01
t-shigeri <fko2447037@stu.o-hara.ac.jp>
2025-01-14 14:03:03 +0900

    new

---
+++ zakotu.py
@@ -0,0 +1,12 @@
+# 数値のリスト
+numbers = [1, 2, 3, 4, 5]
+
+# 合計を計算する関数
+def calculate_sum(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+# 合計を表示
+print("合計:", calculate_sum(numbers))
